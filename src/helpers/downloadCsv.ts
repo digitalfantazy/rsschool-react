11425e2329ad75b13466d10c5eecb4d1967a3097
Артём <artemaa58092002@gmail.com>
2024-08-13 11:57:12 +0200

    init react-forms task

--- src/helpers/downloadCsv.ts
+++
@@ -1,9 +0,0 @@
-export const downloadCsv = (
-  data: { id: number; name: string; ingredients: string[]; instructions: string[] }[],
-) => {
-  const csvContent = data
-    .map((e) => `${e.id},${e.name},${e.ingredients.join(';')},${e.instructions.join(',')}`)
-    .join('\n');
-  const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8' });
-  return URL.createObjectURL(blob);
-};
